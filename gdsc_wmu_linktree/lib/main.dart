18a6aed49f99ce46891cb8941dbb91cdb176eeab
elainechan01 <elaineyunru.chan@wmich.edu>
2023-01-24 00:36:10 -0500

    code init

---
+++ gdsc_wmu_linktree/lib/main.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(const PersonalPortfolioApp());
+}
+
+class PersonalPortfolioApp extends StatelessWidget {
+
+  const PersonalPortfolioApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: PersonalPortfolio(),
+        ),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
+
+class PersonalPortfolio extends StatefulWidget {
+
+  const PersonalPortfolio({Key? key}) : super(key: key);
+
+  
+  @override
+  PersonalPortfolioState createState() => PersonalPortfolioState();
+}
+
+class PersonalPortfolioState extends State<PersonalPortfolio> with TickerProviderStateMixin {
+  
+  final Color appColor = Colors.blueAccent;
+  final String avatarImg = 'https://lh3.googleusercontent.com/a/AEdFTp5Pwl32euYMRqZfkP-BN2dlRbW21gqRM_B8Czmb=s96-c-rg-br100';
+
+  late AnimationController ctrl;
+  late AnimationController contentCtrl;
+  
+  @override
+  void initState() {
+    super.initState();
+    ctrl = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2)
+    )..repeat();
+    
+    contentCtrl = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3)
+    )..forward();
+  }
+
+  @override
+  void dispose() {
+    ctrl.dispose();
+    contentCtrl.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: Tween<double>(begin: 0.0, end: 1.0)
+      .animate(CurvedAnimation(parent: contentCtrl, curve: Curves.easeInOut)),
+      child: SlideTransition(
+      position: Tween<Offset>(begin: const Offset(0.0, 0.125), end: Offset.zero)
+      .animate(CurvedAnimation(parent: contentCtrl, curve: Curves.easeInOut)),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                FadeTransition(
+                  opacity: Tween<double>(begin: 1.0, end: 0.0)
+                  .animate(CurvedAnimation(parent: ctrl, curve: Curves.easeInOut)),
+                  child: ScaleTransition(
+                  scale: Tween<double>(begin: 1.0, end: 1.5).
+                  animate(CurvedAnimation(parent: ctrl, curve: Curves.easeInOut)),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: appColor,
+                      borderRadius: BorderRadius.circular(50),
+                    )
+                  ) 
+                ) 
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: const EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    image: DecorationImage(
+                      image: NetworkImage(avatarImg),
+                      fit: BoxFit.cover
+                    ),
+                    border: Border.all(
+                      color: appColor,
+                      width: 8
+                    )
+                  )
+                )
+              ] 
+            ),
+            Text('Google Developer Student Club', 
+              style: TextStyle(
+                color: appColor, 
+                fontSize: 20, 
+                fontWeight: FontWeight.bold
+              )
+            ),
+            Text('Western Michigan University', 
+              style: TextStyle(
+                color: appColor, 
+                fontSize: 15
+              )
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(width: 10),
+                SizedBox(
+                  height: 100,
+                  child: DefaultTextStyle(
+                      style: const TextStyle(
+                        color: Colors.white, 
+                        fontSize: 50
+                      ),
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          RotateAnimatedText('Made By You'),
+                          RotateAnimatedText('Keep It Real'),
+                          RotateAnimatedText('Our Googley Growth'),
+                        ],
+                      ),
+                    ),
+                )
+              ]
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                launchUrl(Uri.parse('https://www.instagram.com/gdsc_wmu/'));
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  FaIcon(FontAwesomeIcons.instagram),
+                  const SizedBox(width: 10),
+                  Text('Instagram', style: TextStyle(color: appColor))
+                ]
+              )
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                launchUrl(Uri.parse('https://www.linkedin.com/company/google-developer-student-clubs-wmu/'));
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                FaIcon(FontAwesomeIcons.linkedin),
+                const SizedBox(width: 10),
+                Text('LinkedIn', style: TextStyle(color: Colors.blueAccent))
+              ]
+            )
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                launchUrl(Uri.parse('https://gdsc.community.dev/western-michigan-university/'));
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                FaIcon(FontAwesomeIcons.globe),
+                const SizedBox(width: 10),
+                Text('GDSC WMU', style: TextStyle(color: Colors.blueAccent))
+              ]
+            )
+            )
+          ]
+        )
+      )
+    )
+    );
+  }
+}
